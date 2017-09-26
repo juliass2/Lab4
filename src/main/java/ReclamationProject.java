27666cfea77a0d65105d1e212ef0e089cd202287
Hotar <Hotar@LAPTOP-PKIJC30E>
2017-09-26 15:48:41 -0500

    Fixed many bugs and attempted to define what the function does

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,56 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ *  Project which holds a function that finds part of one string within another.
+ */
+public class ReclamationProject {
+    /**
+     * Main function that prints out the result of the function doit(a,b).
+     * @param args main method
+     */
+    public static void main(final String[] args) {
+        String a = "supercalifragilicious";
+        String b = "california";
+        System.out.println(doit(a, b));
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+    }
+    /**
+     * Tells how much of one string is within another.
+     * @param a - first input string
+     * @param b - second input string
+     * @return r - the part of one string that is also in the other (first time)
+     */
+    public static String doit(final String a, final String b) {
+        String localA = a;
+        String localB = b;
+        if (a.length() > b.length()) {
+            String c = a;
+            localA = b;
+            localB = c;
+        } else {
+            localA = a;
+            localB = b;
+        }
+
+        String r;
+        if (localA.equals(localB)) {
+            r = localA;
+        } else {
+            r = "";
+        }
+
         /*
-         * For loop with i
+         * For loop with i. Goes through each letter in each string until there is a match.
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
+        for (int indexOfA = 0; indexOfA < localA.length(); indexOfA++) {
+            for (int backIndexOfA = localA.length() - indexOfA; backIndexOfA > 0; backIndexOfA--) {
+                for (int indexOfB = 0; indexOfB < localB.length() - backIndexOfA; indexOfB++) {
+                    if (localA.regionMatches(indexOfA, localB, indexOfB, backIndexOfA)
+                            && backIndexOfA > r.length()) {
+                        r = localA.substring(indexOfA, indexOfA + backIndexOfA);
+                    }
+                }
+           }
         } return r; }
 }
